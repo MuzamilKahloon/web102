fe29599f1cf4ca7e6fa1d8b94eb46140dee8a60e
MuzamilKahloon <130220559+MuzamilKahloon@users.noreply.github.com>
2023-08-03 06:16:13 -0700

    Add files via upload

---
+++ Responsive website/Net.js
@@ -0,0 +1,9 @@
+burger=document.querySelector('.burger');
+navbar=document.querySelector('.navbar');
+rightnav=document.querySelector('.rightnav');
+navList=document.querySelector('.nav-list');
+burger.addEventListener('click',()=>{
+        rightnav.cc.toggle('v-class-rep');
+        navList.cc.toggle('v-class-rep');
+        navbar.cc.toggle('h-nav-rep');
+})
